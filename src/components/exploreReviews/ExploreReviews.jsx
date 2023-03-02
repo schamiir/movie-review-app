e8dd102e2f6d182561f9888ef1c5f56f3febfb42
JohnEmilio <jl.emilio1@gmail.com>
2023-03-02 15:18:38 -0500

    changes to all reviews page

--- src/components/exploreReviews/ExploreReviews.jsx
+++ src/components/exploreReviews/ExploreReviews.jsx
@@ -1,10 +1,12 @@
 import {React, useState, useEffect} from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import UserHeader from '../userpage/UserHeader'
 import "./ExploreReviews.scss"
 
 
 function ExploreReviews() {
   const [allReviews, setAllReviews] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:8080/reviews/all-users")
@@ -14,6 +16,10 @@ function ExploreReviews() {
 
   function capitalize (str) {
     return str[0].toUpperCase + str.slice(1)
+  }
+
+  const handleUserLink = (e) => {
+    navigate(`/user/${e.target.id}`)
   }
   
   
@@ -29,7 +35,7 @@ function ExploreReviews() {
                 <div className="projcard-title display-6">{movie.movieTitle}</div>
                 <div className="projcard-bar"></div>
                 <div className="projcard-description">
-                  <span className='fw-bolder' style={{fontSize:"1.25rem"}}>Reviewed by: <a href={`/user/${movie.userId}`} id="username-text" className="text-dark">{movie.username}</a></span>
+                  <span className='fw-bolder' style={{fontSize:"1.25rem"}}>Reviewed by: <a onClick={e => handleUserLink(e)} id={movie.userId} className="text-dark">{movie.username}</a></span>
                   <p style={{fontSize:"1rem", color:"black"}}>{movie.review.charAt(0).toUpperCase() + movie.review.slice(1)}</p>
                 </div>
               </div>
